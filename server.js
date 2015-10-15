712c95b3e0dbebbda220102bd75595c8ddbb1e48
Richard Smith <richard.smith.67@gmail.com>
2015-10-15 16:48:41 +0100

    added package.json

--- server.js
+++ server.js
@@ -2,13 +2,9 @@
  * Created by richard.smith on 15/10/2015.
  */
 var express = require('express');
-var bodyParser = require('body-parser');
-
 
 var app = express();
-app.use(bodyParser.json());
 app.use('/js', express.static('js'));
-//app.use('/styles', express.static('styles/bundle'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/main.html')
